d3948f64e7c2a9aea12afa069b0231a4fac6b6f2
LilyHau <leeok2005@gmail.com>
2026-03-23 16:26:55 +0800

    OE video compression

--- src/pages/ourCruisesPage/OurCruisesPage.jsx
+++ src/pages/ourCruisesPage/OurCruisesPage.jsx
@@ -1,60 +1,52 @@
 import { Link } from "react-router-dom";
 import Image from "../../assets/Cruises/cruisesum.png";
-import Image1 from "../../assets/Cruises/OE.PNG";
-import Image2 from "../../assets/Cruises/SD.PNG";
-import Image3 from "../../assets/Cruises/SG.PNG";
+import Image1 from "../../assets/Cruises/OE.webp";
+import Image2 from "../../assets/Cruises/SD.webp";
+import Image3 from "../../assets/Cruises/SG.webp";
 import Line from "../../assets/Cruises/Line.png";
 import Accommodations from "../ourCruisesPage/Accommodations";
-import styles from "./OurCruisesPage.module.css"
+import styles from "./OurCruisesPage.module.css";
 
 const OurCruisesPage = () => {
-
   return (
     <div>
-      <div  className={styles.heroContainer}>
-        <img
-          src={Image}
-          className={styles.heroImage}
-          alt="Cruise Hero"
-        />
-        <div
-        className={styles.thumbWrapper}
-        >
-          <div
-          className={styles.thumbItem}
-          >
+      <div className={styles.heroContainer}>
+        <img src={Image} className={styles.heroImage} alt="Cruise Hero" />
+        <div className={styles.thumbWrapper}>
+          <div className={styles.thumbItem}>
             <Link to="/OCEANEXPLORER">
-              <img src={Image1} className={styles.thumbImage} alt="Ocean Explorer" />
-              <p className={styles.thumbText}>
-                Ocean Explorer
-              </p>
+              <img
+                src={Image1}
+                className={styles.thumbImage}
+                alt="Ocean Explorer"
+              />
+              <p className={styles.thumbText}>Ocean Explorer</p>
             </Link>
           </div>
-          <div
-          className={styles.thumbItem}>
+          <div className={styles.thumbItem}>
             <Link to="/SEAGUARDIAN">
-              <img src={Image3} className={styles.thumbImage} alt="Sea Guardian" />
-              <p className={styles.thumbText}>
-                Sea Guardian
-              </p>
+              <img
+                src={Image3}
+                className={styles.thumbImage}
+                alt="Sea Guardian"
+              />
+              <p className={styles.thumbText}>Sea Guardian</p>
             </Link>
           </div>
-          <div
-          className={styles.thumbItem}>
+          <div className={styles.thumbItem}>
             <Link to="/SERENITYDREAM">
-              <img src={Image2} className={styles.thumbImage} alt="Serenity Dream" />
-              <p
-              className={styles.thumbText}>
-                Serenity Dream
-              </p>
+              <img
+                src={Image2}
+                className={styles.thumbImage}
+                alt="Serenity Dream"
+              />
+              <p className={styles.thumbText}>Serenity Dream</p>
             </Link>
           </div>
         </div>
       </div>
       <div>
-        <img
-        className={styles.dividerLine}
-        src={Line} alt="" />
+        <img className={styles.dividerLine} src={Line} alt="" />
       </div>
       <Accommodations />
     </div>

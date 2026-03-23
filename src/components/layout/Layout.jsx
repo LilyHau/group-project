de30ca710f1d42c68a3e631f6d5f2e2cdb353bf6
LilyHau <leeok2005@gmail.com>
2026-03-23 15:40:27 +0800

    fix layout and our cruises media query

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router-dom";
-import styles from "./Layout.module.scss";
 import Navbar from "../navbar/Navbar";
 import RunningText from "../footer/RunningText";
 import Footer from "../footer/Footer";
@@ -8,16 +7,8 @@ const Layout = ({ cartItems }) => {
   return (
     <>
       <header>
-        <nav className={styles.nav}>
+        <nav>
           <Navbar cartItems={cartItems} />
-          <a href="/">Home</a>
-          <a href="/about">About</a>
-          <a href="/cruises">Our Cruises</a>
-          <a href="/onboard">OnboardThrills</a>
-          <a href="/exclusive">ExclusiveMerch</a>
-          <a href="/FAQSection">FAQSection</a>
-          <a href="/shop">Shop</a>
-          <a href="/cart">Cart</a>
         </nav>
       </header>
       <main>
